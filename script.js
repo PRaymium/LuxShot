6fac3bb4c6450328fccf1e257009c7d497cc0e5f
PRaymium <90473035+PRaymium@users.noreply.github.com>
2021-10-19 23:33:12 +0300

    update form

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let login_button = document.getElementsByClassName('login-button');
-let authorization_form = document.getElementsByClassName('authorization-form');
+var login_button = document.getElementsByClassName('login-button');
+var authorization_form = document.getElementsByClassName('authorization-form');
+var global_container = document.getElementsByClassName('global-container');
 
-login_button.onclick = function() {
-    authorization_form[0].classList.toggle('active');
+login_button[0].onclick = function() {
+    authorization_form[0].classList.toggle("active");
+    global_container[0].classList.toggle("active");
 }
