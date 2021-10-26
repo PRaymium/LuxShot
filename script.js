d6311146be5afe06a6897ec41a12e530eac5f7d6
PRaymium <90473035+PRaymium@users.noreply.github.com>
2021-10-26 22:25:39 +0300

    update form

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var login_button = document.getElementsByClassName('login-button');
-var authorization_form = document.getElementsByClassName('authorization-form');
 var global_container = document.getElementsByClassName('global-container');
 var reg_toggle = document.getElementsByClassName('reg-toggle');
 var login_toggle = document.getElementsByClassName('login-toggle');
@@ -8,21 +7,26 @@ var signup_form = document.getElementsByClassName('signup-form');
 var close_form_button = document.getElementsByClassName('close-form-button');
 
 login_button[0].onclick = function() {
-    authorization_form[0].classList.toggle("active");
+    login_form[0].classList.add("visible");
     global_container[0].classList.toggle("active");
 }
 
 reg_toggle[0].onclick = function() {
-	signup_form[0].classList.toggle("visible");
-	login_form[0].classList.toggle("non-visible")
+	signup_form[0].classList.add("visible");
+	login_form[0].classList.remove("visible")
 }
 
 login_toggle[0].onclick = function() {
-	signup_form[0].classList.toggle("visible");
-	login_form[0].classList.toggle("non-visible")
+	signup_form[0].classList.remove("visible");
+	login_form[0].classList.add("visible")
 }
 
 close_form_button[0].onclick = function() {
-	authorization_form[0].classList.toggle("active");
-	global_container[0].classList.toggle("active");
+	login_form[0].classList.remove("visible");
+	global_container[0].classList.remove("active");
+}
+
+close_form_button[1].onclick = function() {
+	signup_form[0].classList.remove("visible");
+	global_container[0].classList.remove("active");
 }
