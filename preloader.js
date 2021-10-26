9d55121e524460d90ae85a75789ff7e120f51cb3
PRaymium <90473035+PRaymium@users.noreply.github.com>
2021-10-26 21:17:33 +0300

    update preloader, added required flags to form

---
+++ preloader.js
@@ -0,0 +1,5 @@
+let preloader = document.getElementsByClassName('preloader');
+
+window.onload = function(){
+	preloader[0].classList.add("loaded");
+}
--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var login_button = document.getElementsByClassName('login-button');
 var authorization_form = document.getElementsByClassName('authorization-form');
 var global_container = document.getElementsByClassName('global-container');
-var preloader = document.getElementsByClassName('preloader');
 var reg_toggle = document.getElementsByClassName('reg-toggle');
 var login_toggle = document.getElementsByClassName('login-toggle');
 var login_form = document.getElementsByClassName('login-form');
@@ -15,19 +14,15 @@ login_button[0].onclick = function() {
 
 reg_toggle[0].onclick = function() {
 	signup_form[0].classList.toggle("visible");
-	login_form[0].classList.toggle("non_visible")
+	login_form[0].classList.toggle("non-visible")
 }
 
 login_toggle[0].onclick = function() {
 	signup_form[0].classList.toggle("visible");
-	login_form[0].classList.toggle("non_visible")
+	login_form[0].classList.toggle("non-visible")
 }
 
 close_form_button[0].onclick = function() {
 	authorization_form[0].classList.toggle("active");
 	global_container[0].classList.toggle("active");
-}
-
-window.onload = function(){
-	preloader[0].classList.add("loaded");
 }
