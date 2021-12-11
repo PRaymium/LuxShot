837aa8b3c98f7535540453b3135c32af6ed46e3d
PRaymium <90473035+PRaymium@users.noreply.github.com>
2021-12-11 16:42:32 +0300

    syntax update

--- script.js
+++ script.js
@@ -32,71 +32,15 @@ closeFormButton[1].onclick = function() {
 	formContainer.classList.remove("blurred");
 }
 
-
-
-function emailCheck(input){
-	//return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-	input.type = "email";
-	return input.checkValidity();
-}
-
-function passwordCheck(input){
-	input.pattern = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$";
-	return input.checkValidity();
-}
-
-function nameCheck(input){
-	input.pattern = "[А-Яа-я\\s-]*";
-	return input.checkValidity();
-}
-
-function phoneCheck(input){
-	input.pattern = "8\\d{3}\\d{3}\\d{2}\\d{2}";
-	return input.checkValidity();
-}
-
-function signupFormValidation(){
-	var signupFormNameInput = document.getElementById("signupForm-name-input");
-	var signupFormEmailInput = document.getElementById("signupForm-email-input");
-	var signupFormPhoneNumberInput = document.getElementById("signupForm-phone-input")
-	var signupFormPasswordInput = document.getElementById("signupForm-password-input");
-	var signupFormPasswordRepeatInput = document.getElementById("signupForm-passwordRepeat-input");
-	var signupFormCheckboxInput = document.getElementById("signupForm-checkbox-input");
-
-	if (!nameCheck(signupFormNameInput)){
-		console.log("signupFormNameInput incorrect");
-		return false;
-	}
-	if (!emailCheck(signupFormEmailInput)){
-		console.log("signupFormEmailInput incorrect");
-		return false;
-	}
-	if (!passwordCheck(signupFormPasswordInput)){
-		console.log("signupFormPasswordInput incorrect");
-		return false;
-	}
-	if (!passwordCheck(signupFormPasswordRepeatInput)){
-		console.log("signupFormPasswordRepeatInput incorrect");
-		return false;
-	}
-	if (!phoneCheck(signupFormPhoneNumberInput)){
-		console.log("signupFormPhoneNumberInput incorrect");
-		return false;
-	}
-	if (!signupFormCheckboxInput.checked){
-		return false;
-	}
-	return true;
-}
-
 async function authorizationDataSend(e){
 	e.preventDefault();
-	if (e.target.id === "signup-form")
+	if (e.target.id === "signupForm")
 	{
-		if (!signupFormValidation())
-		{
-			return;
-		}
+		if (!(signupForm-passwordRepeat-input.value == signupForm-password-input.value))
+        {
+            alert("Пароли не совпадают");
+            return;
+        }
 	}
 		
 	let formData = new FormData(this);
